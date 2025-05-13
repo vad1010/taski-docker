32d5495d69deabcb5d306fdb606a5c494a856b23
vad1010 <silutinv@yandex.ru>
2025-05-13 20:10:09 +0300

    After add setup.cfg

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,9 +60,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # Добавьте import
-
-
-
 # Этими строчками замените текущую настройку DATABASES
 DATABASES = {
     'default': {
