c2b30f1ffdb4fa3a352d12332c57cbdb478e44e1
vad1010 <silutinv@yandex.ru>
2025-05-14 09:12:34 +0300

    Add test new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.252.140.191', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['84.252.140.191', 'taskivad1010.ddns.net', 'localhost', '127.0.0.1']
 
 
 # Application definition
